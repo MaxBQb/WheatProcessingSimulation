00666234dec8020147491b7e799bfc0c9867ff04
Бабышев Максим <cmdazaza@gmail.com>
2021-11-20 18:06:37 +0300

    + Main menu button states

--- view/main_menu.py
+++ view/main_menu.py
@@ -8,37 +8,46 @@ from view.workers import WorkersView
 
 
 class MainMenuView(base.BaseInteractiveWindow):
+    def __init__(self):
+        super().__init__()
+        self._ways = (
+            (layout.button_workers, self.open_workers_view),
+            (layout.button_roles, self.open_roles_view),
+            (layout.button_machine_types, self.open_machine_types_view),
+            (layout.button_flour_grades, self.open_flour_grades_view),
+            (layout.button_grinding_grades, self.open_grinding_grades_view),
+        )
 
     def build_layout(self):
         self.layout = layout.get_layout()
 
     def set_handlers(self):
         super().set_handlers()
-        self.channel.subscribe(
-            layout.button_workers,
-            self.open_workers_view
-        )
-        self.channel.subscribe(
-            layout.button_roles,
-            self.open_roles_view
-        )
-        self.channel.subscribe(
-            layout.button_machine_types,
-            self.open_machine_types_view
-        )
-        self.channel.subscribe(
-            layout.button_flour_grades,
-            self.open_flour_grades_view
-        )
-        self.channel.subscribe(
-            layout.button_grinding_grades,
-            self.open_grinding_grades_view
-        )
+        for _id, transition in self._ways:
+            self.channel.subscribe(
+                _id,
+                self.mark('Green', False),
+                transition,
+                self.mark('Dark Green', True),
+            )
+
+    def dynamic_build(self):
+        super().dynamic_build()
+        for _id, _ in self._ways:
+            self.window[_id].update(button_color='#555')
 
     def init_window(self, **kwargs):
         super().init_window(**dict(
             size=(640, 400)
         ) | kwargs)
+
+    def mark(self, color: str, is_enabled: bool):
+        def on_event(context: base.Context):
+            for _id, _ in self._ways:
+                self.window[_id].update(disabled=not is_enabled,
+                                        button_color="#555" if is_enabled else "#444")
+            context.element.update(button_color=color)
+        return on_event
 
     @base.transition
     def open_workers_view(self, _):
